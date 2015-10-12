047f2d8346b0c73cba757ecd8c52ff80a340b1b6
Martin Pöhlmann <mpdeimos@users.noreply.github.com>
2015-10-12 11:56:32 +0200

    disabling FMI

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -5,16 +5,16 @@ import java.util.Map.Entry;
 
 import org.apache.http.client.ClientProtocolException;
 
-import spark.Request;
-import spark.Response;
-import spark.Spark;
-
 import com.mpdeimos.foodscraper.Retriever;
 import com.mpdeimos.foodscraper.data.IBistro;
 import com.mpdeimos.foodscraper.data.IDish;
 import com.mpdeimos.foodscraper.data.IMenu;
 import com.mpdeimos.webscraper.ScraperException;
 import com.mpdeimos.webscraper.util.Strings;
+
+import spark.Request;
+import spark.Response;
+import spark.Spark;
 
 public class Server {
 
@@ -23,8 +23,7 @@ public class Server {
 
 	public Server() {
 		Spark.port(config.PORT);
-		Spark.before((request, response) -> response
-				.type("text/plain; charset=UTF-8"));
+		Spark.before((request, response) -> response.type("text/plain; charset=UTF-8"));
 		Spark.get("/", this::getHome);
 		Spark.get("/slack/bistro", slack::sendBistro);
 		Spark.get("/config", (req, res) -> new Config(), new JsonTransformer());
@@ -36,13 +35,16 @@ public class Server {
 		PrintWriter writer = new PrintWriter(out);
 		Iterable<Entry<IBistro, IMenu>> menu = new Retriever().getTodaysMenu();
 		for (Entry<IBistro, IMenu> entry : menu) {
-			writer.println("## " + entry.getKey().getName() + " ##");
-			writer.println(Strings.EMPTY);
-			for (IDish dish : entry.getValue().getDishes()) {
-				writer.println(" " + dish.getName());
-				writer.println(" "
-						+ config.PRICE_FORMAT.format(dish.getPrice()));
+			try {
+				writer.println("## " + entry.getKey().getName() + " ##");
 				writer.println(Strings.EMPTY);
+				for (IDish dish : entry.getValue().getDishes()) {
+					writer.println(" " + dish.getName());
+					writer.println(" " + config.PRICE_FORMAT.format(dish.getPrice()));
+					writer.println(Strings.EMPTY);
+				}
+			} catch (Exception e) {
+
 			}
 		}
 		return out.getBuffer().toString();
@@ -52,12 +54,10 @@ public class Server {
 		return new Config();
 	}
 
-	public Object getUrl(Request req, Response res)
-			throws ClientProtocolException, IOException {
+	public Object getUrl(Request req, Response res) throws ClientProtocolException, IOException {
 		res.type("text/html; charset=UTF-8");
 
-		return org.apache.http.client.fluent.Request
-				.Get(req.queryParams("url")).connectTimeout(5000)
+		return org.apache.http.client.fluent.Request.Get(req.queryParams("url")).connectTimeout(5000)
 				.socketTimeout(5000).execute().returnContent().asString();
 	}
 
